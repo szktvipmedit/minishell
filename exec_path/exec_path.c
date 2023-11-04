90f8a610759e6ad604971000d94828ab3e32df03
Kouta Suzuki <kousuzuk@c1r12s2.42tokyo.jp>
2023-11-04 17:50:37 +0900

    できた

--- exec_path/exec_path.c
+++ exec_path/exec_path.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
-
-int main()
+#include <unistd.h>
+int confirm_exist_path(char *path)
 {
-	printf("hello\n");
+	if(access(path, F_OK) == 0)
+		return 1;
+	return 0;
+}
+
+int exec_path(char *path, char **argv, char **envp)
+{
+	if(!confirm_exist_path(path))
+		return (write(2, "このパスにコマンドはないのでとりあえず１返しとくよ\n", 27), 1);
+	//今はオプションを考えないので、execveの第二引数にNULLを入れます
+	execve(path, argv, envp);
+	return 0;
+}
+
+int main(int argc, char **argv, char **envp)
+{
+	exec_path("/bin/pwd", argv, envp);
 	return 0;
 }

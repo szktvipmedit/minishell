e6152faef67459a5d0e52e75c1bcdf4ac39cd625
JIN YASUKAWA <j.em.in@JINnoMacBook-Air.local>
2023-11-07 03:15:01 +0900

    strcmpを追加

--- libft/libft.h
+++ libft/libft.h
@@ -40,6 +40,7 @@ int			ft_toupper(int c);
 int			ft_tolower(int c);
 char		*ft_strchr(char *s, int c);
 char		*ft_strrchr(char *s, int c);
+int			ft_strcmp(char *s1, char *s2);
 int			ft_strncmp(char *s1, char *s2, size_t n);
 void		*ft_memchr(void *s, int c, size_t n);
 int			ft_memcmp(void *s1, void *s2, size_t n);
